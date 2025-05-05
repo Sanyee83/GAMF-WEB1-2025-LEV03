daba7d4cd0df44374830c0a6b00d55fc7f4bd962
Sanyee83 <rezniks@hotmail.com>
2025-05-05 22:28:57 +0200

    X

--- HTML/js/scripts/db.js
+++ HTML/js/scripts/db.js
@@ -1,6 +1,5 @@
 const apiUrl = 'https://nettuts.hu/jms/Sanyee83/';
 
-export const getAll = (entity = 'users') => 
-    {
-    return  fetch(apiUrl + entity).then(res => res.json());
-    }
+export const getAll = (entity = 'users') => {
+    return fetch(apiUrl + entity).then(res => res.json());
+};
