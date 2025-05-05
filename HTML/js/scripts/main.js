095ba04e51fa8e13b3f848a572ff5e153e054394
Sanyee83 <rezniks@hotmail.com>
2025-05-05 22:20:35 +0200

    Vege

--- HTML/js/scripts/main.js
+++ HTML/js/scripts/main.js
@@ -1,10 +1,8 @@
-import {getAll} from './db.js';
+import { getAll } from './db.js';
 
 const table = document.querySelector('table.table');
 
-
 const generateTable = (data = []) => {
-
     console.log(data);
 
     const thead = document.createElement('thead');
@@ -13,43 +11,37 @@ const generateTable = (data = []) => {
     const keys = Object.keys(data[0]);
     const tr = document.createElement('tr');
     thead.appendChild(tr);
-    
     keys.forEach(key => {
         const th = document.createElement('th');
-        th.appendChild(th);
-        tr.innerText = key;
-
+        tr.appendChild(th);
+        th.innerText = key;
     });
-
     const th = document.createElement('th');
-    th.appendChild(th);
+    tr.appendChild(th);
 
-    
     const tbody = document.createElement('tbody');
     table.appendChild(tbody);
-
-    data.forEach(row => {const keys = Object.keys(data[0]);
+    data.forEach(row => {
         const tr = document.createElement('tr');
         tbody.appendChild(tr);
         keys.forEach(key => {
-            const th = document.createElement('td');
-            th.appendChild(th);
+            const td = document.createElement('td');
+            tr.appendChild(td);
             td.innerText = row[key];
-    
         });
-    
-        const th = document.createElement('td');
-        th.appendChild(td);
+        const td = document.createElement('td');
+        tr.appendChild(td);
 
         const btnGroup = document.createElement('div');
         td.appendChild(btnGroup);
         btnGroup.classList.add('btn-group');
-        const infoBtn = document.createElement('button');        
+
+        const infoBtn = document.createElement('button');
         btnGroup.appendChild(infoBtn);
         infoBtn.classList.add('btn', 'btn-info');
-        infoBtn.innerText = 'Info';
+        infoBtn.innerText = 'info';
+    })
 
-})
 };
 
 getAll().then(data => generateTable(data));
