0632489d4b7f60712b7cfaa5a6d985bba0a047c2
Sanyee83 <rezniks@hotmail.com>
2025-05-05 22:08:18 +0200

    kesz

--- HTML/js/scripts/main.js
+++ HTML/js/scripts/main.js
@@ -1,7 +1,55 @@
 import {getAll} from './db.js';
+
+const table = document.querySelector('table.table');
+
+
 const generateTable = (data = []) => {
 
     console.log(data);
+
+    const thead = document.createElement('thead');
+    table.appendChild(thead);
+
+    const keys = Object.keys(data[0]);
+    const tr = document.createElement('tr');
+    thead.appendChild(tr);
+    
+    keys.forEach(key => {
+        const th = document.createElement('th');
+        th.appendChild(th);
+        tr.innerText = key;
+
+    });
+
+    const th = document.createElement('th');
+    th.appendChild(th);
+
+    
+    const tbody = document.createElement('tbody');
+    table.appendChild(tbody);
+
+    data.forEach(row => {const keys = Object.keys(data[0]);
+        const tr = document.createElement('tr');
+        tbody.appendChild(tr);
+        keys.forEach(key => {
+            const th = document.createElement('td');
+            th.appendChild(th);
+            td.innerText = row[key];
+    
+        });
+    
+        const th = document.createElement('td');
+        th.appendChild(td);
+
+        const btnGroup = document.createElement('div');
+        td.appendChild(btnGroup);
+        btnGroup.classList.add('btn-group');
+        const infoBtn = document.createElement('button');        
+        btnGroup.appendChild(infoBtn);
+        infoBtn.classList.add('btn', 'btn-info');
+        infoBtn.innerText = 'Info';
+
+})
 };
 
 getAll().then(data => generateTable(data));
